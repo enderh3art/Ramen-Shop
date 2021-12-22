79dc7deff8ade17de38a4fef5a6a704a822aaf49
enderh3art <64457282+enderh3art@users.noreply.github.com>
2021-12-21 19:17:16 -0500

    Untracked files issue resolved to fix .gitignore

--- node_modules/vercel/dist/runtimes/go1.x/filename.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getOutputFile() {
-    const ext = process.platform === 'win32' ? '.exe' : '';
-    return `bootstrap${ext}`;
-}
-exports.getOutputFile = getOutputFile;
-//# sourceMappingURL=filename.js.map
--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -193,3 +193,5 @@ export default class Camera
         this.controls.update()
     }
 }
+
+// test
