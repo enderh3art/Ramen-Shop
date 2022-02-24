07cb5a0305e89e0782b3c186bce379a9959fd658
enderh3art <64457282+enderh3art@users.noreply.github.com>
2022-02-24 14:50:33 -0500

    added social picture and destroy function

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -95,37 +95,37 @@ export default class Experience
         this.performance.update()
     }
 
-    // destroy()
-    // {
-    //     this.sizes.off('resize')
-    //     this.time.off('tick')
+    destroy()
+    {
+        this.sizes.off('resize')
+        this.time.off('tick')
 
-    //     // Traverse the whole scene
-    //     this.scene.traverse((child) =>
-    //     {
-    //         // Test if it's a mesh
-    //         if(child instanceof THREE.Mesh)
-    //         {
-    //             child.geometry.dispose()
+        // Traverse the whole scene
+        this.scene.traverse((child) =>
+        {
+            // Test if it's a mesh
+            if(child instanceof THREE.Mesh)
+            {
+                child.geometry.dispose()
 
-    //             // Loop through the material properties
-    //             for(const key in child.material)
-    //             {
-    //                 const value = child.material[key]
+                // Loop through the material properties
+                for(const key in child.material)
+                {
+                    const value = child.material[key]
 
-    //                 // Test if there is a dispose function
-    //                 if(value && typeof value.dispose === 'function')
-    //                 {
-    //                     value.dispose()
-    //                 }
-    //             }
-    //         }
-    //     })
+                    // Test if there is a dispose function
+                    if(value && typeof value.dispose === 'function')
+                    {
+                        value.dispose()
+                    }
+                }
+            }
+        })
 
-    //     this.camera.controls.dispose()
-    //     this.renderer.instance.dispose()
+        this.camera.controls.dispose()
+        this.renderer.instance.dispose()
 
-    //     if(this.debug.active)
-    //         this.debug.ui.destroy()
-    // }
+        if(this.debug.active)
+            this.debug.ui.destroy()
+    }
 }
