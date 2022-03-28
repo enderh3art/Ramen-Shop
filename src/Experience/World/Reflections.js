5a8754e12f1418bff408d0f6b0fb4662d1f4db52
enderh3art <64457282+enderh3art@users.noreply.github.com>
2022-03-28 02:23:41 -0400

    fixed reflector

--- src/Experience/World/Reflections.js
+++ src/Experience/World/Reflections.js
@@ -42,7 +42,7 @@ class Reflector extends Mesh {
             magFilter: THREE.NearestFilter,
             generateMipmaps : false,
             format: THREE.RGBAFormat,
-            // outputEncoding: THREE.sRGBEncoding,
+
             stencilBuffer: false
         };
 
@@ -273,8 +273,8 @@ export default class Reflections
         this.geometry = new THREE.CircleGeometry( 25, 5 );
         this.groundMirror = new Reflector( this.geometry, {
             clipBias: 0.003,
-            textureWidth: window.innerWidth/2,
-            textureHeight: window.innerHeight/2,
+            textureWidth: window.innerWidth,
+            textureHeight: window.innerHeight,
             color: 0x777777
         } );
         this.groundMirror.position.y = -2.8;
